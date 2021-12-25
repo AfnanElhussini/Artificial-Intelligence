42be3cb83208b162583b9068558d6510aa7155f8
Afnan Elhussini <63553675+AfnanElhussini@users.noreply.github.com>
2021-12-26 00:03:20 +0200

    IDDFS

--- main.py
+++ main.py
@@ -1,52 +1,39 @@
-# implementation of Depth Limited First Search Algorithm
-
-# graph representation by using dictionary to act as an adjacency list
 graph = {
     'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F', 'G'],
-    'D': ['H', 'I'],
-    'E': ['J', 'K'],
-    'F': ['L', 'M'],
-    'G': ['N', 'O'],
-    'H': [],
-    'I': [],
-    'J': [],
-    'K': [],
-    'L': [],
-    'M': [],
-    'N': [],
-    'O': []
-
+    'B': ['D','E'],
+    "C": ['G'],
+    'D': [],
+    'E': ['F'],
+    'G': [],
+    'F':[]
 }
 
-# function for dls
-def dls(startnode, goal, path, level , maxdepth):
-    print('\n This is a Current level: ', level)
-    print('Goal node testing for', startnode)
-    path.append(startnode)
-    if startnode == goal:
-        print('Goal test successful')
-        return path
-    print('Goal node testing failed')
-    if level == maxdepth:
+path = list()
+
+def DFS(currentNode,destination,graph,maxDepth,curList):
+    print("Checking for destination",currentNode)
+    curList.append(currentNode)
+    if currentNode==destination:
+        return True
+    if maxDepth<=0:
+        path.append(curList)
         return False
-    print('\nExpanding the current node', startnode)
-    for child in graph[startnode]:
-        if dls(child, goal, path, level + 1, maxdepth):
-            return path
-        path.pop()
+    for node in graph[currentNode]:
+        if DFS(node,destination,graph,maxDepth-1,curList):
+            return True
+        else:
+            curList.pop()
     return False
 
-# Driver Code
-startnode = 'A'
-goal = input('Enter the goal node:- ')
-maxdepth = int(input("Enter the maximum depth limit:-"))
-print()
-path = list()
-res = dls(startnode, goal, path, 0, maxdepth)
-if (res):
-    print("Path to goal node available")
-    print("Path", path)
+def iterativeDDFS(currentNode,destination,graph,maxDepth):
+    for i in range(maxDepth):
+        curList = list()
+        if DFS(currentNode,destination,graph,i,curList):
+            return True
+    return False
+
+if not iterativeDDFS('A','E',graph,4):
+    print("Path is not available")
 else:
-    print("No path available for the goal node in given depth limit")
+    print("A path exists")
+    print(path.pop())
